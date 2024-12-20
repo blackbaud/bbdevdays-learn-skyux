f4c899bd26ac178821373578de8a2c47350455f7
John White <750350+johnhwhite@users.noreply.github.com>
2024-12-20 10:56:39 -0500

    Create HTTP service

---
+++ src/app/services/data/mock-storage.service.ts
@@ -0,0 +1,27 @@
+export class MockStorageService implements Storage {
+  #storage: Record<string, string> = {};
+
+  public get length(): number {
+    return Object.keys(this.#storage).length;
+  }
+
+  public clear(): void {
+    this.#storage = {};
+  }
+
+  public getItem(key: string): string | null {
+    return this.#storage[key] ?? null;
+  }
+
+  public key(index: number): string | null {
+    return Object.keys(this.#storage)[index] ?? null;
+  }
+
+  public removeItem(key: string): void {
+    delete this.#storage[key];
+  }
+
+  public setItem(key: string, value: string): void {
+    this.#storage[key] = value;
+  }
+}
