3efcc7bf9aee1f4ce800eebdc1f03b841a27bf8e
Workshop Script <750350+johnhwhite@users.noreply.github.com>
2025-03-10 09:17:05 +0000

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environment';
+import { WelcomeToSkyuxComponent } from './welcome-to-skyux/welcome-to-skyux.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, WelcomeToSkyuxComponent],
+  styleUrl: './app.component.scss',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+  protected readonly environment = environment;
+}
