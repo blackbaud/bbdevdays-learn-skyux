92a57ab6f33b89d591daa636eb0d8f8771b7cde1
Workshop Script <750350+johnhwhite@users.noreply.github.com>
2025-03-17 09:19:11 +0000

    Create form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,13 @@
 import { Routes } from '@angular/router';
-import { ID } from './types/id';
-import { ViewComponent } from './animal-profiles/view/view.component';
 
 export const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    providers: [
-      {
-        provide: ID,
-        useValue: 'e52947a9-58a5-43fa-9a81-5405bb9c3e58',
-      },
-    ],
-    component: ViewComponent,
+    redirectTo: 'animal-profiles/view/4f24eb2c-deaa-4ee9-bc05-b1d0b614f089',
+  },
+  {
+    path: 'animal-profiles/view/:id',
+    loadComponent: () => import('./animal-profiles/view/view-page.component'),
   },
 ];
