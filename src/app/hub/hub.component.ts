ee0c7c44a7581a8b77642bb4f09da27b006da14b
Workshop Script <750350+johnhwhite@users.noreply.github.com>
2025-03-24 09:22:43 +0000

    Action hub

---
+++ src/app/hub/hub.component.ts
@@ -0,0 +1,72 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { SkyBoxModule } from '@skyux/layout';
+import { SkyActionHubModule } from '@skyux/pages';
+import { map, Observable } from 'rxjs';
+import { welcomeToSkyuxLinks } from '../welcome-to-skyux/welcome-to-skyux-links';
+import { WelcomeToSkyuxComponent } from '../welcome-to-skyux/welcome-to-skyux.component';
+import { DataService } from '../services/data/data.service';
+import { PersistenceService } from '../services/data/persistence.service';
+
+interface NeedsAttentionItem {
+  title: string;
+  permalink: {
+    route?: {
+      commands: string[];
+    };
+    url?: string;
+  };
+}
+
+@Component({
+  selector: 'app-hub',
+  standalone: true,
+  imports: [
+    CommonModule,
+    SkyActionHubModule,
+    WelcomeToSkyuxComponent,
+    SkyBoxModule,
+  ],
+  templateUrl: './hub.component.html',
+  styleUrl: './hub.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class HubComponent {
+  protected readonly loading: Observable<boolean>;
+  protected needsAttention: Observable<NeedsAttentionItem[]>;
+  protected readonly relatedLinks = welcomeToSkyuxLinks.map((link) => ({
+    label: link.label,
+    permalink: {
+      url: link.url,
+    },
+  }));
+  protected readonly title = 'Animal Profiles';
+
+  readonly #dataService = inject(DataService);
+  readonly #persistenceService = inject(PersistenceService);
+
+  constructor() {
+    this.loading = this.#persistenceService.loading;
+    this.needsAttention = this.#dataService.list.pipe(
+      map((profiles) => {
+        const needsAttentionCount = profiles.reduce(
+          (count, profile) => count + (profile.needsAttention ? 1 : 0),
+          0,
+        );
+        const singular = needsAttentionCount === 1;
+        return [
+          {
+            title: `${needsAttentionCount} animal${singular ? '' : 's'} need${singular ? 's' : ''} attention`,
+            permalink: {
+              route: {
+                commands: ['animal-profiles'],
+              },
+            },
+          },
+        ];
+      }),
+    );
+  }
+}
+
+export default HubComponent;
