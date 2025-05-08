e9d8cf7aa4c4168a97f9ab47cceba24dcc2e2e60
Workshop Script <750350+johnhwhite@users.noreply.github.com>
2025-05-08 03:10:48 +0000

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,49 @@
+// @ts-check
+const eslint = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const angular = require('angular-eslint');
+const skyux = require('skyux-eslint');
+const prettier = require('eslint-config-prettier/flat');
+
+module.exports = tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked,
+      ...angular.configs.tsRecommended,
+      ...skyux.configs.tsStrictTypeChecked,
+    ],
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      'no-mixed-operators': 'off',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [
+      ...angular.configs.templateRecommended,
+      ...angular.configs.templateAccessibility,
+      ...skyux.configs.templateRecommended,
+    ],
+    rules: {},
+  },
+  prettier,
+);
