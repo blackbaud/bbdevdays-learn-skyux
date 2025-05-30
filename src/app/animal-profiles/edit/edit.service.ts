c02786c8b55bfc0cee3c4bb94fa735409f5b130f
Workshop Script <750350+johnhwhite@users.noreply.github.com>
2025-05-30 20:33:36 +0000

    Create form

---
+++ src/app/animal-profiles/edit/edit.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, inject } from '@angular/core';
+import { SkyModalService } from '@skyux/modals';
+
+import { ID } from '../../types/id';
+
+import { EditComponent } from './edit.component';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class EditService {
+  #modalService = inject(SkyModalService);
+
+  public edit(id?: string): void {
+    this.#modalService.open(EditComponent, {
+      providers: [
+        {
+          provide: ID,
+          useValue: id ?? '',
+        },
+      ],
+    });
+  }
+}
---
+++ src/app/animal-profiles/view/view-page.component.ts
@@ -0,0 +1,35 @@
+import { Component, inject, input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { SkyIconModule } from '@skyux/icon';
+import { SkyWaitModule } from '@skyux/indicators';
+import { SkyPageModule } from '@skyux/pages';
+
+import { PersistenceService } from '../../services/data/persistence.service';
+import { EditService } from '../edit/edit.service';
+import { parentLink } from '../parent-link';
+
+import { ViewComponent } from './view.component';
+
+@Component({
+  selector: 'app-view-page',
+  templateUrl: './view-page.component.html',
+  imports: [
+    RouterLink,
+    SkyIconModule,
+    SkyPageModule,
+    SkyWaitModule,
+    ViewComponent,
+  ],
+})
+export class ViewPageComponent {
+  public id = input<string>();
+  protected editService = inject(EditService);
+  protected loading = inject(PersistenceService).loading;
+  protected parentLink = parentLink;
+
+  protected edit(): void {
+    this.editService.edit(this.id());
+  }
+}
+
+export default ViewPageComponent;
