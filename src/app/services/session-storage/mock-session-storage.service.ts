41de531bb9bdda35e72c91b6f50deef2a704e088
Workshop Script <750350+johnhwhite@users.noreply.github.com>
2025-08-04 09:28:28 +0000

    Create HTTP service

---
+++ src/app/services/session-storage/mock-session-storage.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+
+/**
+ * Mock service that works like session storage, for testing.
+ */
+@Injectable()
+export class MockSessionStorageService implements Storage {
+  #storage: Record<string, string> = {};
+
+  public get length(): number {
+    return Object.keys(this.#storage).length;
+  }
+
+  public clear(): void {
+    this.#storage = {};
+  }
+
+  public getItem(key: string): string | null {
+    return this.#storage[key] ?? null;
+  }
+
+  public key(index: number): string | null {
+    return Object.keys(this.#storage)[index] ?? null;
+  }
+
+  public removeItem(key: string): void {
+    delete this.#storage[key];
+  }
+
+  public setItem(key: string, value: string): void {
+    this.#storage[key] = value;
+  }
+}
