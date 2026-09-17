787bc085b468ad23ac6de77a03647f898c9c4017
Workshop Script <750350+johnhwhite@users.noreply.github.com>
2026-09-17 09:14:02 +0000

    Create view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { View } from './animal-profiles/view/view';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'animal-profiles/view/c61eea0f-49a0-4213-8ff8-666a696dd93a',
+  },
+  {
+    path: 'animal-profiles/view/:id',
+    component: View,
+  },
+];
---
+++ src/app/types/id.ts
@@ -0,0 +1,6 @@
+import { InjectionToken } from '@angular/core';
+
+/**
+ * This token is used to pass the ID of the animal profile to the edit modal.
+ */
+export const ID = new InjectionToken<string>('ID');
